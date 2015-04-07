80943172eae4418545848f24c162550f79d6048a
Yingjie <1275963@gmail.com>
2015-04-07 15:13:36 +0100

    Now it is static image

--- clear.v
+++ clear.v
@@ -11,7 +11,7 @@ wire rambuffer;
 
 wire CounterXmaxed = (CounterX==8'b10011111); // 159
 wire CounterYmaxed = (CounterY==8'b01110111); // 119
-wire CounterAddress = (address==16'h4B00); // 120*160
+wire CounterAddress = (address==16'h4AFF); // 120*160
 
 ram_background ram_entity(
 	.address(address),
