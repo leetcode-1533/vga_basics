760b14781d58cccefe9061531e7210582d92e315
Yingjie Luan <1275963@gmail.com>
2015-04-26 23:05:59 +0100

    trigger with new design

--- sample_clock.v
+++ sample_clock.v
@@ -4,12 +4,12 @@ module sample_clock(clk_in,clk_out);
 input clk_in;
 output clk_out;
 
-assign clk_out = clk_down;
-reg clk_down = 'b0;
-always @(posedge clk_in)
-begin
-	clk_down = clk_down + 1;
-end
-// assign clk_out = clk_in;
+// assign clk_out = clk_down;
+// reg clk_down = 'b0;
+// always @(posedge clk_in)
+// begin
+// 	clk_down = clk_down + 1;
+// end
+assign clk_out = clk_in;
 
 endmodule
